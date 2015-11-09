461e9aa0d84d37d91a0bc8b00cdb6e9f6efe13f6
Jason Doar <jbdoar@gmail.com>
2015-11-09 11:30:10 -0800

    Added frequency modulated signal

--- pylsl/SendData.py
+++ pylsl/SendData.py
@@ -3,6 +3,7 @@ LSL."""
 
 import random
 import time
+import numpy as np
 
 from pylsl import StreamInfo, StreamOutlet
 
@@ -11,7 +12,13 @@ from pylsl import StreamInfo, StreamOutlet
 # last value would be the serial number of the device or some other more or
 # less locally unique identifier for the stream as far as available (you
 # could also omit it but interrupted connections wouldn't auto-recover).
-info = StreamInfo('BioSemi', 'EEG', 8, 100, 'float32', 'myuid34234')
+info = StreamInfo('BioSemi', 'EEG', 8, 256, 'float32', 'myuid34234')
+
+fc = 10. #Hz the carrier frequency
+fm = .01 #Hz the modulating frequency
+pd = .5 #Hz amplitude of the frequency deviation
+
+signal = np.sin(2*np.pi*fc*time.time()+pd*np.sin(2*np.pi*fm*time.time())/fm)
 
 # next make an outlet
 outlet = StreamOutlet(info)
@@ -20,9 +27,9 @@ print("now sending data...")
 while True:
     # make a new random 8-channel sample; this is converted into a
     # pylsl.vectorf (the data type that is expected by push_sample)
-    mysample = [random.random(), random.random(), random.random(),
-                random.random(), random.random(), random.random(),
-                random.random(), random.random()]
+    mysample = [signal+random.random(), signal+random.random(), signal+random.random(),
+                signal+random.random(), signal+random.random(), signal+random.random(),
+                signal+random.random(), signal+random.random()]
     # now send it and wait for a bit
     outlet.push_sample(mysample)
-    time.sleep(0.01)
+    time.sleep(1./256.)
