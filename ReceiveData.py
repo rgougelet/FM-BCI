aacc66aa8ff55e57a29c499d25a3bbd585a74e6b
rgougelet <freq@fmbci.com>
2015-11-13 14:30:20 -0800

    send and receive functionality working

--- ReceiveData.py
+++ ReceiveData.py
@@ -1,8 +1,15 @@
 """Example program to show how to read a multi-channel time series from LSL."""
+import pylab
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.fftpack
+import random
+import time
+import math
 import sys
 sys.path.insert(0, './pylsl')
-
 from pylsl import StreamInlet, resolve_stream
+import processPAF
 
 # first resolve an EEG stream on the lab network
 print("looking for an EEG stream...")
@@ -11,8 +18,26 @@ streams = resolve_stream('type', 'EEG')
 # create a new inlet to read from the stream
 inlet = StreamInlet(streams[0])
 
+# populate the array in real time
+sampleRate = 512 # make sure this matches the sampleRate in SendData.py
+numOfChannel = 8
+dataLengthSecs = 10
+dataLengthSamples = dataLengthSecs*sampleRate
+voltageSamples = np.empty([numOfChannel,dataLengthSamples])
+sampleIndex = 0
+
 while True:
     # get a new sample (you can also omit the timestamp part if you're not
     # interested in it)
     sample, timestamp = inlet.pull_sample()
-    print(timestamp, sample)
+    
+    # populating the samples
+    voltageSamples[:, sampleIndex] = sample
+    sampleIndex += 1
+
+    if sampleIndex == dataLengthSamples:
+        processPAF(voltageSamples,sampleRate)
+        sampleIndex = 0
+
+
+
--- SendData.py
+++ SendData.py
@@ -1,31 +1,47 @@
-"""Example program to demonstrate how to send a multi-channel time series to
-LSL."""
-
 import random
 import time
-
-import sys
-sys.path.insert(0, './pylsl')
+import math
+import numpy as np
 
 from pylsl import StreamInfo, StreamOutlet
 
 # first create a new stream info (here we set the name to BioSemi,
-# the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
+# the content-type to EEG, 8 channels, 100 Hz(Sampling Rate/per second), and float-valued data) The
 # last value would be the serial number of the device or some other more or
 # less locally unique identifier for the stream as far as available (you
 # could also omit it but interrupted connections wouldn't auto-recover).
-info = StreamInfo('BioSemi', 'EEG', 8, 100, 'float32', 'myuid34234')
+sampleRate = 512.0
+info = StreamInfo('BioSemi', 'EEG', 8, sampleRate, 'float32', 'myuid34234')
 
 # next make an outlet
 outlet = StreamOutlet(info)
 
 print("now sending data...")
 while True:
-    # make a new random 8-channel sample; this is converted into a
-    # pylsl.vectorf (the data type that is expected by push_sample)
-    mysample = [random.random(), random.random(), random.random(),
-                random.random(), random.random(), random.random(),
-                random.random(), random.random()]
+    
+    # channel1 = np.sin(1.0 * 2.0 * np.pi * time.time())  
+    # channel2 = np.sin(5.0 * 2.0 * np.pi * time.time())   
+    # channel3 = np.sin(10.0 * 2.0 * np.pi * time.time())   
+    # channel4 = np.sin(15.0 * 2.0 * np.pi * time.time())   
+    # channel5 = np.sin(10.0 * 2.0 * np.pi * time.time())   
+    # channel6 = np.sin(25.0 * 2.0 * np.pi * time.time())  
+    # channel7 = np.sin(10.0 * 2.0 * np.pi * time.time()) + random.random()
+    # channel8 = random.random()
+    
+    channel1 = 0.1*np.sin(10.0 * 2.0 * np.pi * time.time()) + random.random() 
+    channel2 = 0.2*np.sin(15.0 * 2.0 * np.pi * time.time()) + random.random()  
+    channel3 = 0.3*np.sin(20.0 * 2.0 * np.pi * time.time()) + random.random()  
+    channel4 = 0.4*np.sin(25.0 * 2.0 * np.pi * time.time()) + random.random()  
+    channel5 = 0.5*np.sin(30.0 * 2.0 * np.pi * time.time()) + random.random()  
+    channel6 = 0.6*np.sin(35.0 * 2.0 * np.pi * time.time()) + random.random()
+    channel7 = 0.7*np.sin(40.0 * 2.0 * np.pi * time.time()) + random.random()
+    channel8 = 0.8*np.sin(45.0 * 2.0 * np.pi * time.time()) + random.random()
+    
+    # each channel have a voltage value over time.
+    mysample = [channel1, channel2, channel3,
+                channel4, channel5, channel6,
+                channel7, channel8]
+
     # now send it and wait for a bit
     outlet.push_sample(mysample)
-    time.sleep(0.01)
+    time.sleep(1.0/sampleRate)
