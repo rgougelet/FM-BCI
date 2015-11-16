fba5fcdec61361ce347aecddc0f6d9a8bcf5f40c
rgougelet <freq@fmbci.com>
2015-11-16 10:46:18 -0800

    fixed merge

--- SendData.py
+++ SendData.py
@@ -1,5 +1,3 @@
-"""Example program to demonstrate how to send a multi-channel time series to
-LSL."""
 import random
 import time
 import math
@@ -7,33 +5,20 @@ import numpy as np
 from pylsl import StreamInfo, StreamOutlet, vectorf
 
 for clearline in range(1,100):
-<<<<<<< HEAD
-    print('\n')
-=======
 	print('\n')
->>>>>>> 478254401f8da067fdeabd50367aee17c2cde29a
-
+	
 sampleRate = 512.0
 info = StreamInfo('SimulatedEEG', 'EEG', 8, sampleRate, 'float32', 'myuid34234')
-
-#
 
 numOfChannel = 8
 channelWeights = np.linspace(1./numOfChannel,1,numOfChannel) 
 np.random.shuffle(channelWeights)
 print 'Channel weights:', '\n'
 for channelIndex in range(numOfChannel):
-<<<<<<< HEAD
     print "        ", channelIndex+1, "   ", channelWeights[channelIndex]
 alphaCenter = 10.   # Hz the carrier frequency
 alphaModFreq = 0.1  # Hz the modulating frequency
 alphaFreqDev = 1    # Hz of the frequency deviation
-=======
-	print "        ", channelIndex+1, "   ", channelWeights[channelIndex]
-alphaCenter = 10.   # Hz the carrier frequency
-alphaModFreq = 0.1  # Hz the modulating frequency
-alphaFreqDev = 1	# Hz of the frequency deviation
->>>>>>> 478254401f8da067fdeabd50367aee17c2cde29a
 snr = 2             # signal / noise
 noiseMean = 0
 noiseStdDev = 0.5
@@ -50,8 +35,6 @@ sample = np.empty(numOfChannel)           # create (blank) data array w/ entry f
 print("\n \nSending data...")
 while True:
 
-<<<<<<< HEAD
-    
     # Constructs 1/f noise by iteratively adding normal random noise, effectively the CDF of normal dist.
     nextRandSample = previousRandSample + np.random.normal(noiseMean, noiseStdDev)
     
@@ -73,30 +56,3 @@ while True:
     while now < start + 1/sampleRate:
         now = time.clock()
         pass
-    
-=======
-	
-	# Constructs 1/f noise by iteratively adding normal random noise, effectively the CDF of normal dist.
-	nextRandSample = previousRandSample + np.random.normal(noiseMean, noiseStdDev)
-	
-	# Frequency modulated alpha rhythm, a sinusoidal baseband signal
-	alpha = alphaAmp*np.sin( alphaCenter  * 2.0 * np.pi * time.time() + \
-	alphaFreqDev*np.sin(2 * np.pi * alphaModFreq * time.time()) / alphaModFreq)
-	
-	# assign the weighted alpha rhythm + 1/f noise + additional random noise to each channel in sample
-	for channelIndex in range(0,numOfChannel):
-		sample[channelIndex] = channelWeights[channelIndex]*alpha + nextRandSample + 0.05*np.random.random()	
-	
-	# push sample
-	outlet.push_sample(sample)
-	
-	previousRandSample = nextRandSample 
-	
-	start = time.clock()
-	now = time.clock()
-	while now < start + 1/sampleRate:
-		now = time.clock()
-		pass
-	
-	
->>>>>>> 478254401f8da067fdeabd50367aee17c2cde29a
