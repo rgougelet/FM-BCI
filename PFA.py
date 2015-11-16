1174c8a4e98a1e27d0e75ec62bf6d81973039483
AlexOuyang <ouyangchenxing@hotmail.com>
2015-11-16 10:35:24 -0800

    recording eeg to txt

--- PFA.py
+++ PFA.py
@@ -8,57 +8,47 @@ import random
 import time
 import math
 
-def processingPAF(voltageSamples,sampleRate):
 
-<<<<<<< HEAD
+def processPAF(voltageSamples, sampleRate):
     # Number of samplepoints
     numOfChannel = voltageSamples.shape[0]     # number of channels
     dataLengthSamples = voltageSamples.shape[1]        # sample size
-    sampleSpacing = 1.0 / sampleRate        
-    dataLengthSecs = dataLengthSamples/sampleRate
+    sampleSpacing = 1.0 / sampleRate
+    dataLengthSecs = dataLengthSamples / sampleRate
     desiredFreqResolution = 0.01
-    paddingMultiple = 1/desiredFreqResolution
-    fftLengthSamples = dataLengthSamples*int(paddingMultiple)
-    nyq = 0.5*sampleRate
-    
-=======
+    paddingMultiple = 1 / desiredFreqResolution
+    fftLengthSamples = dataLengthSamples * int(paddingMultiple)
+    nyq = 0.5 * sampleRate
 
-# voltage = matrix[numOfChannel,numOfSamplesPerSecond]
-def processingPAF(voltageSamples, timeStamp):
-    # Number of samplepoints
-    numOfChannel = voltageSamples.shape[0]     # number of channels
-    samples = voltageSamples.shape[1]          # sample size
-
-    sampleSpacing = 1.0 / samples       # sample spacing
-    t = np.linspace(0.0, samples * sampleSpacing, samples)
-
-    # print out time stamp
-    print("Time: ", timeStamp)
-
->>>>>>> 8269aee04574e7b156d55170f2f4e5e563106e05
-    # going through each channel to plot fft result 
-    for channelIndex in range(0,numOfChannel):
+    # going through each channel to plot fft result
+    for channelIndex in range(0, numOfChannel):
 
         # detrend and window channel
-        channelVoltage = voltageSamples[channelIndex,:] - np.mean(voltageSamples[channelIndex,:])
+        channelVoltage = voltageSamples[
+            channelIndex, :] - np.mean(voltageSamples[channelIndex, :])
         #windowed = channelVoltage * signal.blackmanharris(dataLengthSamples)
-        windowed = channelVoltage * signal.gaussian(dataLengthSamples, std=8,sym=False)
-        
+        windowed = channelVoltage * \
+            signal.gaussian(dataLengthSamples, std=8, sym=False)
+
         # compute fft
-        amp = abs(scipy.fftpack.rfft(channelVoltage,fftLengthSamples))
-        freqs = scipy.fftpack.rfftfreq(fftLengthSamples,sampleSpacing)
+        amp = abs(scipy.fftpack.rfft(channelVoltage, fftLengthSamples))
+        freqs = scipy.fftpack.rfftfreq(fftLengthSamples, sampleSpacing)
 
         # find peak frequency
         maxAmplitudeIndex = np.argmax(amp)
         true_maxAmplitudeIndex = parabolic(np.log(amp), maxAmplitudeIndex)[0]
         maxFreq = sampleRate * maxAmplitudeIndex / fftLengthSamples
         true_maxFreq = sampleRate * true_maxAmplitudeIndex / fftLengthSamples
-        print('Channel '+str(channelIndex+1)+':     '+str(true_maxFreq)+'  '+str(maxFreq))
+        print('Channel ' + str(channelIndex + 1) + ':     ' +
+              str(true_maxFreq) + '  ' + str(maxFreq))
 
-        # do not run this cuz you are gonna get bombarded with plots
-<<<<<<< HEAD
-        
-        # plot the figures
+
+        # target.write('Channel ' + str(channelIndex + 1) + ':     ' +
+        #       str(true_maxFreq) + '  ' + str(maxFreq))
+        # target.write("/n")
+
+        # # do not run this cuz you are gonna get bombarded with plots
+        # # plot the figures
         # fig=plt.figure(figsize=(12, 9))
         # ax1=fig.add_subplot(211)
         # fig.suptitle('Channel '+str(channelIndex+1), fontsize=20)
@@ -75,27 +65,3 @@ def processingPAF(voltageSamples, timeStamp):
         # ax2.grid()
 
         # plt.show()
-=======
-
-        # plot the figures
-        fig=plt.figure(figsize=(12, 9))
-        fig.suptitle(('channel ' + repr(channelIndex + 1)), fontsize=14, fontweight='bold')
-        ax1=fig.add_subplot(211)
-        # ax1.set_title('axes title')
-        ax1.set_xlabel('Time [s]')
-        ax1.set_ylabel('Voltage [V]')
-        ax1.plot(t, voltage)
-        ax1.grid()
-
-        ax2=fig.add_subplot(212)
-        ax2.set_xlabel('Frequency [Hz]')
-        ax2.set_ylabel('Amplitude')
-        # show to top 128 frequencies
-        ax2.plot(fx_bins[0:128], abs(fy[0:128]))
-        ax2.plot(fx_bins[maxAmplitude], abs(fy[maxAmplitude]), 'rD')   # highest frequency marker
-        ax2.grid()
-
-        plt.show()
->>>>>>> 8269aee04574e7b156d55170f2f4e5e563106e05
-
-
--- ReceiveData.py
+++ ReceiveData.py
@@ -1,4 +1,3 @@
-"""Example program to show how to read a multi-channel time series from LSL."""
 import pylab
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,42 +7,34 @@ import time
 import math
 import sys
 sys.path.insert(0, './pylsl')
-from pylsl import StreamInlet, resolve_stream
-from PFA import processingPAF
+from pylsl import StreamInlet, resolve_stream, vectorf
+import processPAF
 
 # first resolve an EEG stream on the lab network
 print("looking for an EEG stream...")
-streams = resolve_stream('type', 'EEG')
+streams = resolve_stream('name', 'SimulatedEEG')
 
 # create a new inlet to read from the stream
 inlet = StreamInlet(streams[0])
 
 # populate the array in real time
-sampleRate = 512 # make sure this matches the sampleRate in SendData.py
+sampleRate = 512. # make sure this matches the sampleRate in SendData.py
 numOfChannel = 8
-dataLengthSecs = 10
+dataLengthSecs = 1
 dataLengthSamples = dataLengthSecs*sampleRate
 voltageSamples = np.empty([numOfChannel,dataLengthSamples])
 sampleIndex = 0
+sample = vectorf()
 
 while True:
     # get a new sample (you can also omit the timestamp part if you're not
     # interested in it)
     sample, timestamp = inlet.pull_sample()
-
+    
     # populating the samples
     voltageSamples[:, sampleIndex] = sample
     sampleIndex += 1
 
-<<<<<<< HEAD
     if sampleIndex == dataLengthSamples:
-        processingPAF(voltageSamples,sampleRate)
+        processPAF.processPAF(voltageSamples,sampleRate)
         sampleIndex = 0
-=======
-    if sampleIndex == 256:
-        processingPAF(voltageSamples, timestamp)
-        # sampleIndex = 0
->>>>>>> 8269aee04574e7b156d55170f2f4e5e563106e05
-
-
-
