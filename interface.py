3e46c5e77209f07a9fb1a79b4d90a2c2f0c2e174
Christian Kothe <christiankothe@gmail.com>
2015-11-30 17:12:57 -0800

    first run with alex

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 from framework.latentmodule import LatentModule
 import imp
-pylsl = imp.load_source('pylsl.py', 'C:\Users\ces\Dropbox\LSE\Code\SNAP\src\modules\FM-BCI\pylsl\pylsl.py')
+#pylsl = imp.load_source('pylsl.py', 'C:\Users\ces\Dropbox\LSE\Code\SNAP\src\modules\FM-BCI\pylsl\pylsl.py')
+pylsl = imp.load_source('pylsl.py', 'C:\Users\ces\Documents\SNAP_FMBCI\src\modules\FM-BCI\pylsl\pylsl.py')
+
 import random
 import time
 
@@ -35,7 +37,7 @@ class Main(LatentModule):
 
         while True:
             bci, timestamp = inlet.pull_sample()
-            self.marker("NewFrame_"+str(bci)+"_"+str(timestamp)
-            rect1 = self.rectangle([-0.4,-0.6,(-bci[0]*1.5)-0.75,-0.75],duration=1000,color=[0,0,0,1],block=False)
-            rect2 = self.rectangle([0.4,0.6,(-bci[0]*1.5)-0.75,-0.75],duration=1,color=[0,0,0,1],block=True)
+            self.marker("NewFrame_"+str(bci)+"_"+str(timestamp))
+            rect1 = self.rectangle([-0.4,-0.6,(bci[0]*1.5)-0.75,-0.75],duration=1000,color=[0,0,0,1],block=False)
+            rect2 = self.rectangle([0.4,0.6,(bci[0]*1.5)-0.75,-0.75],duration=1,color=[0,0,0,1],block=True)
             rect1.destroy()
