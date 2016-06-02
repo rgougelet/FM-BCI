b09e9f308ec6f611079eed54c2c569f1ca58ff3f
Ravi Patel <r.patel46825@gmail.com>
2016-06-01 22:00:09 -0700

    added max to Plots

--- Demos/Plots.m
+++ Demos/Plots.m
@@ -16,13 +16,15 @@ Z(99,59) = [2.48431986415799];
 
 disp('Error 1: minimum MSE occurs at neigs = 21, eign_mag = 0.95, MSE = [2.81370667385737e-14]')
 
-figure(1)
-surfc(X,Y,Z)
-title('Error 1')
-xlabel('neigs')
-ylabel('eigv\_mag')
-zlabel('mse')
-hold on
+% figure(1)
+% surfc(X,Y,Z)
+% title('Error 1')
+% xlabel('neigs')
+% ylabel('eigv\_mag')
+% zlabel('mse')
+% hold on
+
+zmax = .0005;
 
 figure(2)
 surfl(X,Y,Z)
@@ -30,33 +32,35 @@ title('Error 1')
 xlabel('neigs')
 ylabel('eigv\_mag')
 zlabel('mse')
+zlim([0,zmax])
 hold on
 
+c = uicontrol;
 %% Error 2: at negis = 63, eig_mag = .96
 
-X2 = 63*ones(96,1);     %X2 represents neigs
-Y2 = [0:.01:.95]';      %Y2 is the eign_mag
-Z2 = zeros([96,96]);    %Z2 is MSE
-for j = 1:96
-    for i = 1:96
-        Z2(i,j) = out2MSE(i);
-    end
-end
-
-disp('Error 2: minimum MSE occurs at neigs = 63, eign_mag = .2, MES = [1.43713002308106e-10]')
-
-figure(3)
-surfc(X2,Y2,Z2)
-title('Error 2')
-xlabel('neigs')
-ylabel('eigv\_mag')
-zlabel('mse')
-hold on
-
-figure(4)
-surfl(X2,Y2,Z2)
-title('Error 2')
-xlabel('neigs')
-ylabel('eigv\_mag')
-zlabel('mse')
-hold on
+% X2 = 63*ones(96,1);     %X2 represents neigs
+% Y2 = [0:.01:.95]';      %Y2 is the eign_mag
+% Z2 = zeros([96,96]);    %Z2 is MSE
+% for j = 1:96
+%     for i = 1:96
+%         Z2(i,j) = out2MSE(i);
+%     end
+% end
+% 
+% disp('Error 2: minimum MSE occurs at neigs = 63, eign_mag = .2, MES = [1.43713002308106e-10]')
+% 
+% figure(3)
+% surfc(X2,Y2,Z2)
+% title('Error 2')
+% xlabel('neigs')
+% ylabel('eigv\_mag')
+% zlabel('mse')
+% hold on
+% 
+% figure(4)
+% surfl(X2,Y2,Z2)
+% title('Error 2')
+% xlabel('neigs')
+% ylabel('eigv\_mag')
+% zlabel('mse')
+% hold on
