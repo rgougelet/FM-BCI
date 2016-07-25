a09a1745d235d0ea44651abed8bba5c564051d83
Robert Gougelet <rgougele@gmail.com>
2016-07-25 14:40:50 -0700

    updated performance function

--- MATLAB/performanceMat.m
+++ MATLAB/performanceMat.m
@@ -1,15 +1,19 @@
-function [ errSort err2 ] = performanceMat( freq, amp, target )
-%detect the number of peaks and the length from the target frequencies 
-[Y, idex1]=max(amp);
-amp(idex1)=0;
-[Y, idex2]=max(amp);
-SortIdex=sort([idex1 idex2]);
-SortTarget=sort(target);
-errSort = (SortTarget-freq(SortIdex)).^2/length(target);
-for i=1:length(target)
-    
+function [ errSort, errPerm ] = performanceMat( freq, amp, target )
+%detect the number of peaks and the length from the target frequencies
 
-    
+%errSort output
+[Y, I]=sort(amp);
+IndexFreq= I((end-length(target)+1):end);
+SortTarget=sort(target);
+%errSort = (SortTarget-freq(IndexFreq)).^2/length(target);
+SubtractMat=[];
+for i=1:length(IndexFreq)
+    SubtractMat=[SubtractMat, SortTarget-freq(IndexFreq(i))];
+end
+% SubtractMat=SubtractMat';
+% SubtractMat=reshape(SubtractMat',length(target),length(target));
+errSort= mean(SubtractMat(1:length(target):end).^2);
+errPerm = mean(SubtractMat.^2);
 
 end
 
